267edcca336c73e5d3c227d06539365c0a8987f3
Harbs <harbsprog@gmail.com>
2019-09-12 23:36:31 -0300

    Query & Route Params

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const server = express();
 
-// localhost:3000/teste
+server.get("/test/:id", (req, res) => {
+  return res.json({ message: `Searching, ${req.params.id}` });
+});
 
-server.get("/test", (req, res) => {
-  return res.json({ message: "Hello World!" });
+server.get("/test/", (req, res) => {
+  return res.json({ message: `Hello, ${req.query.nome}` });
 });
 
 server.listen(3000);
